6a7761bdb68317546bdcf22673b3d0ec1922119b
kimber <kimber.gee@gmail.com>
2016-05-31 19:59:29 -0400

    complete user sign in and out, fixed views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  private
+
+  def require_sign_in
+    unless current_user
+      flash[:error] = "You must be logged in to do that"
+  #	  redirect_to new_session_path
+ 		  redirect_to new_user_session_path
+    end
+  end
 end
