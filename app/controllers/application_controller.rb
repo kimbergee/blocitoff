27bd9dd8c34d111aa53fbabd4c66ccfcf7315c85
kimber <kimber.gee@gmail.com>
2016-05-31 19:59:29 -0400

    complete user sign in and out, fixed views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password, :remember_me) }
   end
+
+  private
+
+  def require_sign_in
+    unless current_user
+      flash[:error] = "You must be logged in to do that"
+		  redirect_to new_user_session_path
+    end
+  end
 end
